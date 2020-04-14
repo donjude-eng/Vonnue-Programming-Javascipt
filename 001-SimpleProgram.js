cbbd51c82efb6a0dfa850f30b53ba847a50f66ee
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-15 03:29:23 +0530

    Program to print the array

--- 001-SimpleProgram.js
+++ 001-SimpleProgram.js
@@ -1,2 +1,5 @@
-const name = ["Red", "Blue", "Green", "yellow"];
-console.log (name.toString());
+var array = ['color' , 12 , (3>5) , 'yellow'];
+console.log (array [0]);
+console.log (array [1]);
+console.log (array [2]);
+console.log (array [3]);
