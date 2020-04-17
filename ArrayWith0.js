a45ff5697fe85402fd22a44c766532bc08a360ad
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-18 01:55:07 +0530

    Program to replace all the elements of an array with 0

---
+++ ArrayWith0.js
@@ -0,0 +1,12 @@
+//3. Write a function to replace all the elements of an array with 0.
+
+const replaceArrayElementsWithZero = () => {
+    sampleArray = ['1', '2', '3', '4'];
+    for (i=0 ; i<4 ; i++) {
+        sampleArray [i] = sampleArray[i] * 0 ;
+    }
+    console.log (sampleArray);
+
+}
+
+replaceArrayElementsWithZero ();
