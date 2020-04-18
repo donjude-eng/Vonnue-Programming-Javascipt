f8aca76ba7c4d8ddf4916232e78cdb521e57d15c
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-19 00:03:47 +0530

    Program to print the array in reverse order

---
+++ 009-reverseArray.js
@@ -0,0 +1,11 @@
+///Print the array in reverse order
+
+const reverseArray = () => {
+    sampleArray = ['1', '2', '3', '4', '5', '6'];
+    for (i = 6; i > 0; i--) {
+        console.log(sampleArray[i])
+
+    }
+}
+
+reverseArray();
