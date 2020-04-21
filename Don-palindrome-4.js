0701e097289238388f426fdfea369b060d988c8f
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-21 23:32:13 +0530

    Update Don-palindrome-4.js

--- Don-palindrome-4.js
+++ Don-palindrome-4.js
@@ -1,6 +1,14 @@
-const palindrome =(str) =>{
-    return true;
-  }
-  palindrome("level");
 
-  const str =prompt ("Enter the word?")
+const palindromeCheck = (teststr) => {
+    const stringLength = testStr.length;
+    const roundedLength = Math.round(stringLength / 2);
+    let palindrome = true;
+    for (let i = 0; i <= roundedLength; i++) {
+        if (testStr[i] !== testStr[stringLength - 1 - i]) {
+            palindrome = false;
+            break;
+        }
+    }
+    const str =prompt ("Enter the word?")
+    console.log(`The string is ${palindrome ? "palindrome" : "not palindrome"}`);
+}
