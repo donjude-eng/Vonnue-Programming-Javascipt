e6bab8ddec82145f1373309f4c27ec6ae1741d63
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-23 01:32:57 +0530

    Update 005-Multiple3and.js

--- 005-Multiple3and.js
+++ 005-Multiple3and.js
@@ -1,9 +1,13 @@
-const printMultiple3and5 = () => {
-    let sampleArray = ['1', '2', '3', '4', '5', '6', '10'];
+const readline = require('readline-sync');
+
+const printMultiple3and5 = (arrayElements) => {
+    let sampleArray = arrayElements.split (',');
     for (let i = 0; i < sampleArray.length; i++); {
         if ((sampleArray[i] % 3 === 0) || (sampleArray[i] % 5 === 0)) {
             console.log(sampleArray[i]);
         }
     }
 }
-printMultiple3and5();
+
+let arrayElements = readline.question("Enter the arrayElements ?");
+printMultiple3and5(arrayElements);
