017f3e2ec19e51adc87fbf496a67f64fe3110962
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-23 01:13:30 +0530

    Updated code

--- 008-ArrayEqualorNot.js
+++ 008-ArrayEqualorNot.js
@@ -1,10 +1,14 @@
 //8.Write a program to compare the current item in the array to the next item and print if they are equal or not.
-const printMultiple3and5 = () => {
-    let sampleArray = ['1', '2', '3', '4', '5', '6', '10'];
-    for (let i = 0; i < sampleArray.length; i++); {
-        if ((sampleArray[i] % 3 === 0) || (sampleArray[i] % 5 === 0)) {
-            console.log(sampleArray[i]);
+const readline = require('readline-sync');
+
+const equalItem = (arrayElements) => {
+    let sampleArray = arrayElements.split(',');
+    for (let i = 1; i < sampleArray.length; i++) {
+        if (sampleArray[i] === sampleArray[i - 1]) {
+            console.log(sampleArray[i], sampleArray[i - 1]);
         }
     }
 }
-printMultiple3and5();
+
+let arrayElements = readline.question("Enter the arrayElements ?");
+equalItem(arrayElements);
