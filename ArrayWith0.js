e762ff833d1401bced430f1bcd4775c66b15105e
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-23 08:41:34 +0530

    Update ArrayWith0.js

--- ArrayWith0.js
+++ ArrayWith0.js
@@ -1,12 +1,16 @@
 //3. Write a function to replace all the elements of an array with 0.
 
+
+const readline = require('readline-sync');
+
 const replaceArrayElementsWithZero = () => {
-    sampleArray = ['1', '2', '3', '4'];
-    for (i=0 ; i<4 ; i++) {
+    let sampleArray = arrayElements.split (',');
+    for (i=0 ; i<sampleArray.length ; i++) {
         sampleArray [i] = sampleArray[i] * 0 ;
     }
     console.log (sampleArray);
 
 }
 
+let arrayElements = readline.question("Enter the arrayElements ?");
 replaceArrayElementsWithZero ();
