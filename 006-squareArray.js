976967654d182e330d675e882809bc179fb8fafc
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-04-23 19:23:42 +0530

    Update 006-squareArray.js

--- 006-squareArray.js
+++ 006-squareArray.js
@@ -1,11 +1,15 @@
 ///6 -Write a function to multiply elements of an array to itself ( find the square of each elements in the array)
 
-const squareArrayElements = () => {
-    sampleArray = ['1', '2', '3', '4', '5', '6'];
-    for (i = 0; i < 6; i++) {
+const readline = require('readline-sync');
+
+const squareArrayElements = (arrayElements) => {
+    let sampleArray = arrayElements.split(',');
+    for (let i = 0; i < sampleArray.length; i++) {
         sampleArray[i] = Math.pow(sampleArray[i], 2);
         console.log(sampleArray[i])
     }
 
 }
-squareArrayElements();
+
+let arrayElements = readline.question("Enter the arrayElements ?");
+squareArrayElements(arrayElements);
