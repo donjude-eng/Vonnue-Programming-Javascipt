aab969a6bf44441858256d3b70e3e2f03bad6f75
Don Jude Joseph <61147320+donjude-eng@users.noreply.github.com>
2020-05-04 22:33:57 +0530

    Code is corrected and updated

--- Don-palindrome-4.js
+++ Don-palindrome-4.js
@@ -1,16 +1,16 @@
 const readline = require ("readline-sync");
 
-const palindromeCheck = (teststr) => {
-    const stringLength = testStr.length;
+const palindromeCheck = (lineTest) => {
+    const stringLength = lineTest.length;
     const roundedLength = Math.round(stringLength / 2);
     let palindrome = true;
     for (let i = 0; i <= roundedLength; i++) {
-        if (testStr[i] !== testStr[stringLength - 1 - i]) {
+        if (lineTest[i] !== lineTest[stringLength - 1 - i]) {
             palindrome = false;
             break;
         }
     }
     console.log(`The word ${palindrome ? "palindrome" : "not palindrome"}`);
 }
-const testStr = readline.question(" Enter the string : ");
-palindromeCheck(testStr);
+const lineTest = readline.question(" Enter the string : ");
+palindromeCheck(lineTest);
